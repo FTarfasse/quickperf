5850e7b2cc55a498312fefd5ad4bc6ffbbf1d1b0
Fabrice Tarfasse <fabrice.tarfasse@gmail.com>
2020-12-04 16:04:59 +0100

    draft PrintWriterBuilder

--- sql/sql-annotations/src/main/java/org/quickperf/sql/execution/AnalyzeSqlVerifier.java
+++ sql/sql-annotations/src/main/java/org/quickperf/sql/execution/AnalyzeSqlVerifier.java
@@ -2,7 +2,10 @@ package org.quickperf.sql.execution;
 
 import org.quickperf.issue.PerfIssue;
 import org.quickperf.issue.VerifiablePerformanceIssue;
+import org.quickperf.sql.SqlExecutions;
 import org.quickperf.sql.annotation.AnalyzeSql;
+import org.quickperf.sql.select.analysis.SelectAnalysis;
+import org.quickperf.sql.select.analysis.SelectAnalysisExtractor;
 
 public class AnalyzeSqlVerifier implements VerifiablePerformanceIssue<AnalyzeSql, SqlAnalysis> {
 
@@ -11,11 +14,23 @@ public class AnalyzeSqlVerifier implements VerifiablePerformanceIssue<AnalyzeSql
     private AnalyzeSqlVerifier() {}
 
     @Override
-    public PerfIssue verifyPerfIssue(AnalyzeSql annotation, SqlAnalysis measure) {
-        long number = measure.getSelectAnalysis().getSelectNumber().getValue();
+    public PerfIssue verifyPerfIssue(AnalyzeSql annotation, SqlAnalysis sqlAnalysis) {
+        //SelectAnalysis selectAnalysis = SelectAnalysisExtractor.INSTANCE.extractPerfMeasureFrom(sqlExecutions);
+
+        long number = sqlAnalysis.getSelectAnalysis().getSelectNumber().getValue();
+
+        System.out.println("JDBC execution: " + sqlAnalysis.getJdbcQueryExecutionNumber().getValue());
+
+        Long selectNumber = sqlAnalysis.getSelectAnalysis().getSelectNumber().getValue();
+        System.out.println("SELECT: " + selectNumber);
+       /*
         if (number > 0) {
             return new PerfIssue(number + " select detected");
         }
+        */
+
+        // "[JDBC QUERY EXECUTION (executeQuery, executeBatch, ...)]"
+        //sqlExecutions.toString();
 
         return PerfIssue.NONE;
     }
